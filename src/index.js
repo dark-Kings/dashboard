79cdafccfe5dbe36a43c5b8492b1da10c9df46f3
Anshul Pant <anshulpant14@gmail.com>
2023-06-25 21:54:15 +0530

    25 june 1 commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { ContextProvider } from './contexts/ContextProvider';
+import { ContextProvider } from './contexts/ContextProvider';
+import { registerLicense } from '@syncfusion/ej2-base';
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <ContextProvider> */}
-      <App />
-    {/* </ContextProvider> */}
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCeUx3WmFZfV1gc19HYFZRTGYuP1ZhSXxQdkNhXX9ZcXRVTmRfV0E=');
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ContextProvider>
+//       <App />
+//     </ContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render( <React.StrictMode>
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+</React.StrictMode>);
